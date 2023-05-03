9dc5513a8aea14a54903a7601fd5224dfe765735
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2023-05-03 18:34:14 +0000

    Finished the exercice 1 theme 1

---
+++ Theme-1/Exercice-1.cc
@@ -0,0 +1,29 @@
+/*
+======================================
+EJERCICIO 1:
+======================================
+*/
+/**
+ * Dado un vector de tamaño 1 invertir este haciendo uso de punteros para ello.
+*/
+
+#include <iostream>
+
+int main() {
+  int vector[] = {1,2,3,4,5,6,7};
+  int *vector_start, *vector_finish;
+  int auxiliary;
+  vector_start = vector; // Apunta a la primera posición del vector
+  vector_finish = vector + 6; // Apunta a la última posición del vector
+
+  while (vector_start < vector_finish) {
+    auxiliary = *vector_start; // De esta manera es como se obtiene el dato propio de la posición a la que apunta un puntero
+    *vector_start = *vector_finish;
+    *vector_finish = auxiliary;
+    vector_start++;
+    vector_finish--;
+  }
+}
+
+// Hay que tener en cuenta que cuando se hace uso directamente de * en los punteros se accede al contenido al que apunta este.
+// en cambio si hacemos uso de & estamos accediendo a la dirección de memoria que apunta dicho puntero.
